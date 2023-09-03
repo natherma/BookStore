569b1a601dcf3315e24e23287b374ac88611f1c9
natherma <nathermanafees786@gmail.com>
2023-09-03 19:29:22 +0530

    Completed backend and started working on frontend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@ import {PORT} from "./config.js"
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import {book} from "./models/bookModels.js"
+import cors from "cors"
+
 dotenv.config()
 
 let app = express();
 
 app.use(express.json())
+app.use(cors({
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders:['Content-Type']
+}))
 
 app.get('/',(req,res)=>
 {
@@ -62,6 +68,60 @@ app.get("/allbooks/:id",async(req,res)=>
         res.status(500).send({message:error.message})
     }
 })
+
+
+app.put('/updateBook/:id', async(req,res)=>
+{
+    const {id}  = req.params
+    try{
+        if(!req.body.title||!req.body.author||!req.body.publishYear)
+        {
+            res.status(400).send("requied fieled missing");
+        }
+        else
+        {
+            const updateBook = await book.findByIdAndUpdate(id,req.body)
+            if(!updateBook)
+            {
+               res.status(400).json({"message":"book was not found"})
+            }
+            else
+            {
+                res.status(200).json({"message":"book was updated"})
+            }
+        }
+    }
+    catch(error) 
+    {
+        console.log(error.message);
+        res.status(500).send({message:error.message})
+    }
+
+})
+
+
+app.delete("/deletebook/:id",async(req,res)=>
+{
+    let {id} = req.params;
+    try{
+        const deletebook = await book.findByIdAndDelete(id);
+        if(!deletebook)
+            {
+               res.status(400).json({"message":"book was not found"})
+            }
+            else
+            {
+                res.status(200).json({"message":"book was deleted"})
+            }
+    }
+    catch(error) 
+    {
+        console.log(error.message);
+        res.status(500).send({message:error.message})
+    }
+})
+
+
 
 
 
